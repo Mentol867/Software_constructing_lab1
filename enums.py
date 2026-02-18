d5d0ea77f8f64cec937b16c85d44f7c4f3544e65
Vyacheslav Shuliak <ipz242_shvv1@student.ztu.edu.ua>
2026-02-18 11:30:15 +0200

    added .md file

---
+++ enums.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+class UserRole(Enum):
+    ADMIN = 'admin'
+    MANAGER = 'manager'
+    USER = 'user'
+
+class BookingStatus(Enum):
+    NEW = 'New'
+    CONFIRMED = 'Confirmed'
+    CANCELED = 'Canceled'
+    COMPLETED = 'Completed'
+
+class CarStatus(Enum):
+    AVAILABLE = 'Available'
+    MAINTENANCE = 'Maintenance'
+    BOOKED = 'Booked'
